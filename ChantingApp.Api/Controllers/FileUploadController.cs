dff47e7793c79788b36492727c00f89135cf6773
Gerald Chifanzwa <g.chifanzwa@theahoyapp.com>
2022-12-08 14:22:59 +0200

    Added file upload api to upload background images

---
+++ ChantingApp.Api/Controllers/FileUploadController.cs
@@ -0,0 +1,23 @@
+﻿using ChantingApp.Api.Handlers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChantingApp.Api.Controllers;
+
+[ApiController, Route("file-upload")]
+public class FileUploadController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public FileUploadController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> UploadFile(IFormFile file)
+    {
+        var url = await _mediator.Send(new UploadFileRequest(file));
+        return url;
+    }
+}
---
+++ ChantingApp.Api/Handlers/UploadImageHandler.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using MediatR;
+
+namespace ChantingApp.Api.Handlers;
+
+public record UploadFileRequest(IFormFile File) : IRequest<string>;
+
+public class UploadImageHandler : IRequestHandler<UploadFileRequest, string>
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<UploadImageHandler> _logger;
+
+    public UploadImageHandler(IConfiguration configuration,
+                              ILogger<UploadImageHandler> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public async Task<string> Handle(UploadFileRequest request, CancellationToken cancellationToken)
+    {
+        return await UploadFile(request.File);
+    }
+
+    private async Task<string> UploadFile(IFormFile file)
+    {
+        var connectionString = _configuration.GetConnectionString("AzureStorageAccount");
+        var blobServiceClient = new BlobServiceClient(connectionString);
+        var containerClient = blobServiceClient.GetBlobContainerClient("chant-background-images");
+
+        var fileName = Path.Combine(Guid.NewGuid().ToString(), file.FileName);
+        var blobClient = containerClient.GetBlobClient(fileName);
+
+        await using var stream = file.OpenReadStream();
+        await blobClient.UploadAsync(stream);
+        var fileUrl = blobClient.Uri.AbsoluteUri;
+        return fileUrl;
+    }
+
+    
+}
