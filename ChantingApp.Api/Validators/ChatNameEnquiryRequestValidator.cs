5d5ae18b629c249538804fd948a4f8645c7368d0
Gerald Chifanzwa <g.chifanzwa@theahoyapp.com>
2022-12-08 14:30:54 +0200

    Improved Validation for input models

---
+++ ChantingApp.Api/Validators/ChatNameEnquiryRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using ChantingApp.Api.ViewModels;
+using FluentValidation;
+
+namespace ChantingApp.Api.Validators;
+
+public class ChatNameEnquiryRequestValidator : AbstractValidator<ChatNameEnquiryRequest>
+{
+    public ChatNameEnquiryRequestValidator()
+    {
+        RuleFor(x => x.Name)
+           .NotEmpty()
+           .MinimumLength(3);
+    }
+}
---
+++ ChantingApp.Api/Validators/EndStreamViewModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using ChantingApp.Api.ViewModels;
+using FluentValidation;
+
+namespace ChantingApp.Api.Validators;
+
+public class EndStreamViewModelValidator: AbstractValidator<EndStreamViewModel>
+{
+    public EndStreamViewModelValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+    }
+}
